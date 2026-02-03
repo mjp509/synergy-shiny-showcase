00ae6c4120660c3c21258378794f28fd7e12f1e2
mjp509 <mjp509@usask.ca>
2026-02-02 18:18:19 -0600

    Fix base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: '/synergy-shiny-showcase/',
   build: {
     target: 'esnext',
     rollupOptions: {
